1a9b62b30a7a8e644af9519d521199e81cc0d730
esther <esther@esther-Latitude-D830>
2015-11-24 17:17:23 +0100

    progress Screenshots

--- src/com/grafr/ScreenshotHandeler.java
+++ src/com/grafr/ScreenshotHandeler.java
@@ -9,17 +9,31 @@ import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
 public class ScreenshotHandeler {
+	
+	String format = "png";
+	String name = "Default";
+	
+	public void upload(Component c){
+		BufferedImage image;
+		image = getBufferedImage(c);
+		
+	}
+	
 
+
+	private BufferedImage getBufferedImage(Component c){
+		BufferedImage image = new BufferedImage(c.getWidth(), c.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		return image;
+	}
+	
 	public void writeToDisc(Component c) {
-		String format = "png";
-		String name = "Default";
 		try {
 			printComponent(c, format, name);
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(Grafr.graph, "Error", "", JOptionPane.ERROR_MESSAGE);
 		}
 	}
-
+	
 	private void printComponent(Component c, String format, String filename) throws IOException {
 		//File f = new File(filename);
 		// Create a renderable image with the same width and height as the
