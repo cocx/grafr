4b87b378c100b4c5a8835a8d9994037e474325de
TFDLokyar <max.doesburg@hotmail.com>
2015-12-01 16:40:13 +0100

    Added read/write function The idea is the user can input a number to save the file to similar to a save slot. possibly with drop down box.

--- src/com/grafr/SaveLoadHandeler.java
+++ src/com/grafr/SaveLoadHandeler.java
@@ -1,13 +1,59 @@
 package com.grafr;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
 
 public class SaveLoadHandeler {
-	void saveToFile(String filename, String [] content){
-		int savecount = 0;
-		File save = new File("/saves/test"+ savecount +".txt");
-		while (save.exists()){
-			
+	String filename = "save";
+
+	public void saveContentToFile(String content, int number) {
+		File save;
+		try {
+			save = new File("saves/" + filename + number + ".txt");
+			if (save.exists()) {
+				int a = JOptionPane.showConfirmDialog(null,
+						"Save already in use" + System.lineSeparator()
+						+ "Do you wish to overwrite?", "",
+						JOptionPane.ERROR_MESSAGE);
+				System.out.println(a);
+				if (a == 0) {
+					save.createNewFile();
+				} else {
+					return;
+				}
+			}
+			FileWriter fw = new FileWriter(save.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+	}
+
+	public String readContentFromFile(int number) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("saves/"
+					+ filename + number + ".txt"));
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
+			return sb.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
